51245de20845fccdb66d20381015dd76e08938a9
brunty <openblue555@gmail.com>
2014-08-27 10:52:35 +0100

    Sorted some tests out

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -13,17 +13,6 @@ return array(
             'database' => __DIR__.'/../../../db/testing.sqlite',
             'prefix'   => '',
         ),
-
-        'mysql' => array(
-            'driver'    => 'mysql',
-            'host'      => $_ENV['DB_HOST'],
-            'database'  => $_ENV['DB_NAME'],
-            'username'  => $_ENV['DB_USER'],
-            'password'  => $_ENV['DB_PASS'],
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ),
     ),
 
 
--- app/tests/GenSend/Controllers/SendControllerTest.php
+++ app/tests/GenSend/Controllers/SendControllerTest.php
@@ -1,13 +1,16 @@
 <?php namespace GenSend\Tests\Controllers;
+use Illuminate\Support\Facades\Request;
 use Testcase;
 use Route;
 
 class SendControllerTest extends TestCase {
+
     public function setUp() {
         parent::setUp();
         $app = $this->createApplication();
         $app->make('artisan')->call('migrate');
         Route::enableFilters();
+        Request::setSession($this->app['session.store']); // needed otherwise RuntimeException is thrown as we're using Session in the view file for things like errors etc.
     }
     /**
      * A basic functional test example.
@@ -16,6 +19,10 @@ class SendControllerTest extends TestCase {
      */
     public function testIndexIsOk()
     {
+        $this->client->restart();
+        $this->call('GET', 'send');
+
+        $this->assertResponseOk();
 
     }
 }
